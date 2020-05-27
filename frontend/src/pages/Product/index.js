440ef9984424362be07704fadb396154638ba4a0
Thiago Dias Nogueira <42389093+tdnogueira@users.noreply.github.com>
2020-05-27 02:36:29 -0300

    Add files via upload

--- frontend/src/pages/Product/index.js
+++ frontend/src/pages/Product/index.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { Link, useHistory } from "react-router-dom";
-import { FiPower, FiTrash2 } from "react-icons/fi";
+import { FiPower, FiTrash2, FiThumbsUp, FiThumbsDown } from "react-icons/fi";
 
 import api from "../../services/api";
 import logoImg from "../../assets/logo.svg";
 
-export default function Profile() {
+export default function Product() {
   const [products, setProducts] = useState([]);
+  const [status, setStatus] = useState("");
+  const [approvedby_id, setApprovedby_id] = useState("");
 
   const history = useHistory();
 
   const productId = localStorage.getItem("productId");
   const userToken = localStorage.getItem("userToken");
+  const userId = localStorage.getItem("userId");
 
   useEffect(() => {
     api
@@ -25,6 +28,29 @@ export default function Profile() {
         setProducts(response.data);
       });
   }, [productId, userToken]);
+
+  async function handleStatusProduct(e, id, stat) {
+    e.preventDefault();
+
+    setStatus(status, stat);
+    setApprovedby_id(approvedby_id, userId);
+    const data = {
+      status,
+      approvedby_id,
+    };
+
+    try {
+      await api.put(`products/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+
+      history.push("/product");
+    } catch (err) {
+      alert(`${err} ao aprovar/reprovar produto, tente novamente.`);
+    }
+  }
 
   async function handleDeleteProduct(id) {
     try {
@@ -47,42 +73,64 @@ export default function Profile() {
   }
 
   return (
-    <div className='profile-container'>
-      <header>
-        <img src={logoImg} alt='Super Now' />
-        <span>Bem vindo ao Super Now</span>
+    <div className='product-container'>
+      <head></head>
+      <body>
+        <header>
+          <img src={logoImg} alt='Super Now' />
+          <span>Bem vindo ao Super Now</span>
 
-        <Link className='button' to='/products/new'>
-          Novo produto
-        </Link>
-        <button onClick={handleLogout} type='button'>
-          <FiPower size={18} color='#E02041' />
-        </button>
-      </header>
+          <Link className='button' to='/products/new'>
+            Novo produto
+          </Link>
+          <button onClick={handleLogout} type='button'>
+            <FiPower size={18} color='#E02041' />
+          </button>
+        </header>
+        <main>
+          <h1>Produtos cadastrados</h1>
 
-      <h1>Produtos cadastrados</h1>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>
+                <strong>PRODUTO:</strong>
+                <p>{product.title}</p>
 
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <strong>PRODUTO:</strong>
-            <p>{product.title}</p>
+                <strong>DESCRIÇÃO:</strong>
+                <p>{product.description}</p>
 
-            <strong>DESCRIÇÃO:</strong>
-            <p>{product.description}</p>
+                <strong>STATUS:</strong>
+                <p>{product.status}</p>
 
-            <strong>STATUS:</strong>
-            <p>{product.status}</p>
+                <button
+                  onClick={() => handleDeleteProduct(product.id)}
+                  type='button'
+                >
+                  <FiTrash2 size={20} color='#a8a8b3' />
+                </button>
 
-            <button
-              onClick={() => handleDeleteProduct(product.id)}
-              type='button'
-            >
-              <FiTrash2 size={20} color='#a8a8b3' />
-            </button>
-          </li>
-        ))}
-      </ul>
+                <approve
+                  onClick={(e) =>
+                    handleStatusProduct(e, product.id, "aprovado")
+                  }
+                  type='button'
+                >
+                  <FiThumbsUp size={20} color='#a8a8b3' />
+                </approve>
+
+                <reprove
+                  onClick={(e) =>
+                    handleStatusProduct(e, product.id, "reprovado")
+                  }
+                  type='button'
+                >
+                  <FiThumbsDown size={20} color='#a8a8b3' />
+                </reprove>
+              </li>
+            ))}
+          </ul>
+        </main>
+      </body>
     </div>
   );
 }
